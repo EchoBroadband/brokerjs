65211c35eb2ff0557ecba5507d41cd3c901e48ec
Michael Whitaker <mikey.whitaker@gmail.com>
2016-03-04 17:15:51 -0700

    Implemented cancel feature. Oops! Made .on callbacks more javascripty. Instead of a single data object, it passes data as arguments on the callback. Increased code coverage.

--- test/BrokerJSTest.js
+++ test/BrokerJSTest.js
@@ -7,7 +7,7 @@ let util = require('util');
 
 let Broker, broker;
 
-const VERSION = '0.6.1';
+const VERSION = '0.6.2';
 
 /** @test {Broker} */
 describe('Broker', function() {
@@ -127,42 +127,63 @@ describe('Broker', function() {
 			channel = 'new:no:callback:with:options';
 			assert.throws(function(){
 				subId = broker.on(channel, null, {priority:1});
+			}, function(e){
+				if(/Callback or Subscrition Id required/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error ('+channel+').');
 
 			// Invalid callback with null options:
 			channel = 'new:no:callback:null:options';
 			assert.throws(function(){
 				subId = broker.on(channel, null);
+			}, function(e){
+				if(/Callback or Subscrition Id required/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error ('+channel+').');
 
 			// Invalid callback with no options:
 			channel = 'new:no:callback:no:options';
 			assert.throws(function(){
 				subId = broker.on(channel);
+			}, function(e){
+				if(/Callback or Subscrition Id required/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error ('+channel+').');
 
 			// Invalid callback with null options:
 			channel = 'new:string:callback:null:options';
 			assert.throws(function(){
 				subId = broker.on(channel, 'broken!', null);
+			}, function(e){
+				if(/Callback or Subscrition Id required/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error ('+channel+').');
 
 			// Invalid callback with null options:
 			channel = 'new:number:callback:null:options';
 			assert.throws(function(){
 				subId = broker.on(channel, 12358, null);
+			}, function(e){
+				if(/Callback or Subscrition Id required/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error ('+channel+').');
 
 			// Invalid callback with no options:
 			channel = 'new:string:callback:no:options';
 			assert.throws(function(){
 				subId = broker.on(channel, 'broken!');
+			}, function(e){
+				if(/Callback or Subscrition Id required/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error ('+channel+').');
 
 			// Invalid callback with no options:
 			channel = 'new:number:callback:no:options';
 			assert.throws(function(){
 				subId = broker.on(channel, 12358);
+			}, function(e){
+				if(/Callback or Subscrition Id required/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error ('+channel+').');
 		});
 
@@ -174,16 +195,25 @@ describe('Broker', function() {
 			channel = null;
 			assert.throws(function(){
 				subId = broker.on(channel, function(){}, {priority:1});
+			}, function(e){
+				if(/Valid channelId required in order to subscribe/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error (null with options and callback).');
 
 			// missing channel with options and callback
 			assert.throws(function(){
 				subId = broker.on(function(){}, {priority:1});
+			}, function(e){
+				if(/Valid channelId required in order to subscribe/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error (missing with options and callback).');
 
 			// number channel with options and callback
 			assert.throws(function(){
 				subId = broker.on(12358, function(){}, {priority:1});
+			}, function(e){
+				if(/Valid channelId required in order to subscribe/.test(e))
+					return true;
 			}, 'Invalid callback function did not throw an error (number with options and callback).');
 
 		});
@@ -198,16 +228,25 @@ describe('Broker', function() {
 			channel = 'new:string:options';
 			assert.throws(function(){
 				subId = broker.on(channel, mycallback, 'blahblahblah');
+			}, function(e){
+				if(/Passed options must be an object or null/.test(e))
+					return true;
 			}, 'String options did not throw an error ('+channel+').');
 			
 			channel = 'new:number:options';
 			assert.throws(function(){
 				subId = broker.on(channel, mycallback, 12358);
+			}, function(e){
+				if(/Passed options must be an object or null/.test(e))
+					return true;
 			}, 'Number options did not throw an error ('+channel+').');
 
 			channel = 'new:function:options';
 			assert.throws(function(){
 				subId = broker.on(channel, mycallback, function(){});
+			}, function(e){
+				if(/Passed options must be an object or null/.test(e))
+					return true;
 			}, 'Function options did not throw an error ('+channel+').');
 		});
 
@@ -314,7 +353,7 @@ describe('Broker', function() {
 
 			// Verify completion of emission at promise resolution.
 			result.then(function(){
-				assert(emitted == true, 'Event was never emitted ('+chanId+')');
+				assert.equal(emitted, true, 'Event was never emitted ('+chanId+')');
 				done();
 			}).catch(done);
 		});
@@ -326,22 +365,23 @@ describe('Broker', function() {
 			let data = {name:'bob'};
 
 			// Listen to what's emitted.
-			broker.on(chanId, function(event) {
+			broker.on(chanId, function(data, fish, event) {
 				assert(event.channelId == 'emit:valid:id:and:data', 'ChannelId was incorrect.');
 				assert(event.subscription.channelId == 'emit:valid:id:and:data', 'Subscription channelId was incorrect.');
-				assert(event.data, 'Data was null ('+chanId+') ' + util.inspect(event));
-				assert(event.data.name == 'bob', 'Data was wrong ('+chanId+')');
+				assert(data, 'Data was null ('+chanId+') ' + util.inspect(event));
+				assert(data.name == 'bob', 'Data was wrong ('+chanId+')');
+				assert.equal(fish, 'fish', 'Fish was wrong ('+chanId+')');
 				emitted = true;
 			});
 
-			let result = broker.emit(chanId, data);
+			let result = broker.emit(chanId, data, 'fish');
 
 			// Make sure we're returned a promise.
 			assert(result instanceof Promise, 'Emit did not return a promise ('+chanId+').');
 
 			// Verify completion of emission at promise resolution.
 			result.then(function(){
-				assert(emitted === true, 'Event was never emitted ('+chanId+')');
+				assert.equal(emitted, true, 'Event was never emitted ('+chanId+')');
 				done();
 			}).catch(done);
 		});
@@ -377,7 +417,7 @@ describe('Broker', function() {
 
 			// Verify completion of emission at promise resolution.
 			result.then(function() {
-				assert(emitted === true, 'Event was never emitted ('+chanId+')');
+				assert.equal(emitted, true, 'Event was never emitted ('+chanId+')');
 				done();
 			}).catch(done);
 		});
@@ -389,11 +429,11 @@ describe('Broker', function() {
 			let data = {name:'bob'};
 
 			// Listen to what's emitted.
-			broker.on(chanId, function(event) {
+			broker.on(chanId, function(data, event) {
 				assert(event.channelId == 'emit:with:random:id', 'ChannelId was incorrect.');
 				assert(event.subscription.channelId == 'emit:with:*', 'Subscription channelId was incorrect.');
-				assert(event.data, 'Data was null ('+chanId+') ' + util.inspect(event));
-				assert(event.data.name == 'bob', 'Data was wrong ('+chanId+')');
+				assert(data, 'Data was null ('+chanId+') ' + util.inspect(event));
+				assert.equal(data.name, 'bob', 'Data was wrong ('+chanId+')');
 
 				emitted = true;
 			});
@@ -405,7 +445,7 @@ describe('Broker', function() {
 
 			// Verify completion of emission at promise resolution.
 			result.then(function(){
-				assert(emitted === true, 'Event was never emitted ('+chanId+')');
+				assert.equal(emitted, true, 'Event was never emitted ('+chanId+')');
 				done();
 			}).catch(done);
 
@@ -452,6 +492,57 @@ describe('Broker', function() {
 				assert(testedOrder == correctOrder, 'Tested order was not in order [' + testedOrder + '] ('+chanId+').');
 				done();
 			}).catch(done);
+		});
+
+		it('should emit with an object containing a channelId', function(done) {
+			// Check default expected arguments:
+			let chanId = 'emit:valid:id';
+			let emitted = false;
+
+			// Listen to what's emitted.
+			broker.on(chanId, function(a, b, c, event) {
+				assert(event.channelId == 'emit:valid:id', 'ChannelId was incorrect.');
+				assert(event.subscription.channelId == 'emit:valid:id', 'Subscription channelId was incorrect.');
+
+				assert.equal(a, 'a', 'Emitted data "a" does not match.');
+				assert.equal(b.name, '42', 'Emitted data "b" does not match.');
+				assert.equal(c, 3, 'Emitted data "c" does not match.');
+
+				emitted = true;
+			});
+
+			let result = broker.emit(chanId, 'a', {name:'42'}, 3);
+
+			// Make sure we're returned a promise.
+			assert(result instanceof Promise, 'Emit did not return a promise ('+chanId+').');
+
+			// Verify completion of emission at promise resolution.
+			result.then(function(){
+				assert.equal(emitted, true, 'Event was never emitted ('+chanId+')');
+				done();
+			}).catch(done);
+		});
+
+		it('should cancel a subscription', function(done) {
+			// Check default expected arguments:
+			let chanId = 'emit:with:cancel';
+			let emitted = 0;
+
+			broker.subscribe(chanId, function(event) {
+				event.cancelled = true;
+				emitted++;
+			});
+
+			broker.on(chanId, function(event) {
+				throw(new Error('Event should have been cancelled!'));
+				emitted++;
+			}, {priority: 10});
+
+			let result = broker.emit(chanId);
+			result.then(()=>{
+				assert.equal(emitted, 1, 'Event was not cancelled!');
+				done();
+			})
 		});
 
 		it('should re-order channel subscription lists when forced options are applied', function(done) {
@@ -548,6 +639,42 @@ describe('Broker', function() {
 			}).catch(done);
 		});
 
+		it('should fail on wrong count type', function() {
+			let chanId = 'bob';
+
+			// Count is an object
+			assert.throws(function() {
+				broker.on(chanId, function(){}, {count: {}});
+			}, function(e){
+				if(/Options.count must be a number/.test(e))
+					return true;
+			}, 'broker.on does not fail when count is an object.');
+
+			// Count is a function
+			assert.throws(function() {
+				broker.on(chanId, function(){}, {count: function(){}});
+			}, function(e){
+				if(/Options.count must be a number/.test(e))
+					return true;
+			}, 'broker.on does not fail when count is a function.');
+
+			// Count is an object
+			assert.throws(function() {
+				broker.on(chanId, function(){}, {count: 'Stringyyyy'});
+			}, function(e){
+				if(/Options.count must be a number/.test(e))
+					return true;
+			}, 'broker.on does not fail when count is a string.');
+
+			// Count is an object
+			assert.throws(function() {
+				broker.on(chanId, function(){}, {count: /reg/});
+			}, function(e){
+				if(/Options.count must be a number/.test(e))
+					return true;
+			}, 'broker.on does not fail when count is a regex.');
+		});
+
 		it('should unsubscribe after count reaches zero', function(done) {
 			// Check default expected arguments:
 			let chanId = 'emit:with:count:3';
@@ -561,17 +688,17 @@ describe('Broker', function() {
 			result1 = broker.emit(chanId);
 			// Verify completion of emission at promise resolution.
 			result1.then(function() {
-				assert(emitted == 1, 'Event is out of sync with count 1 ('+chanId+') count: '+emitted);
+				assert.equal(emitted, 1, 'Event is out of sync with count 1 ('+chanId+') count: '+emitted);
 
 				result2 = broker.emit(chanId);
 				// Verify completion of emission at promise resolution.
 				result2.then(function() {
-					assert(emitted == 2, 'Event is out of sync with count 2 ('+chanId+') count: '+emitted);
+					assert.equal(emitted, 2, 'Event is out of sync with count 2 ('+chanId+') count: '+emitted);
 					result3 = broker.emit(chanId);
 		
 					// Verify completion of emission at promise resolution.
 					result3.then(function() {
-						assert(emitted == 3, 'Event is out of sync with count 3 ('+chanId+') count: '+emitted);
+						assert.equal(emitted, 3, 'Event is out of sync with count 3 ('+chanId+') count: '+emitted);
 						
 						// Make sure the subscription has been removed.
 						let subs = broker.getSubscribers();
@@ -584,29 +711,40 @@ describe('Broker', function() {
 			}).catch(done);
 		});
 
-
-		it('should fail with an invalid channelId', function() {
+		it('should fail with an invalid or missing channelId', function() {
 			let result;
 
 			// Test missing channel id and data.
-			assert.throws(function(){
+			assert.throws(function() {
 				result = broker.emit();
-			}, 'Missing channelId on "emit" did not throw an error.');
+			}, function(e){
+				if(/Invalid channelIdOrOpts provided to emit/.test(e))
+					return true;
+			}, 'No channelId on "emit" did not throw an error.');
 
 			// Test with number channel id.
-			assert.throws(function(){
+			assert.throws(function() {
 				result = broker.emit(12358);
+			}, function(e){
+				if(/Invalid channelIdOrOpts provided to emit/.test(e))
+					return true;
 			}, 'Number channelId on "emit" did not throw an error.');
 
 			// Test with function channel id.
-			assert.throws(function(){
+			assert.throws(function() {
 				result = broker.emit(function(){});
+			}, function(e){
+				if(/Invalid channelIdOrOpts provided to emit/.test(e))
+					return true;
 			}, 'Function channelId on "emit" did not throw an error.');
 
 			// Test with an object channel id.
-			assert.throws(function(){
+			assert.throws(function() {
 				result = broker.emit({});
-			}, 'Function channelId on "emit" did not throw an error.');			
+			}, function(e){
+				if(/Missing or invalid channelId provided in emit options/.test(e))
+					return true;
+			}, 'Empty Object channelId on "emit" did not throw an error.');
 		});
 
 
@@ -627,34 +765,58 @@ describe('Broker', function() {
 		it('should fail when passed incorrect arguments', function(){
 			assert.throws(function(){
 				broker.off();
+			}, function(e){
+				if(/callback, callback, or subscriptionId required in order to unsubscribe/.test(e))
+					return true;
 			}, '.off did not throw error with no exceptions.');
 
 			assert.throws(function() {
 				broker.off(123, function(){});
+			}, function(e){
+				if(/channelIdOrsubId must be a string in order to unsubscrib/.test(e))
+					return true;
 			}, '.off did not throw error with number + function combo.');
 
 			assert.throws(function() {
 				broker.off(123);
+			}, function(e){
+				if(/channelIdOrsubId must be a string in order to unsubscrib/.test(e))
+					return true;
 			}, '.off did not throw error with number.');
 
 			assert.throws(function() {
 				broker.off({}, function(){});
+			}, function(e){
+				if(/channelIdOrsubId must be a string in order to unsubscrib/.test(e))
+					return true;
 			}, '.off did not throw error with object + function combo.');
 
 			assert.throws(function() {
 				broker.off({});
+			}, function(e){
+				if(/channelIdOrsubId must be a string in order to unsubscrib/.test(e))
+					return true;
 			}, '.off did not throw error with object.');
 
 			assert.throws(function() {
 				broker.off({}, 123);
+			}, function(e){
+				if(/channelIdOrsubId must be a string in order to unsubscrib/.test(e))
+					return true;
 			}, '.off did not throw error with object + number combo.');
 
 			assert.throws(function() {
 				broker.off(123, {});
+			}, function(e){
+				if(/channelIdOrsubId must be a string in order to unsubscrib/.test(e))
+					return true;
 			}, '.off did not throw error with number + object combo.');
 
 			assert.throws(function() {
 				broker.off(function(){});
+			}, function(e){
+				if(/channelIdOrsubId must be a string in order to unsubscrib/.test(e))
+					return true;
 			}, '.off did not throw error with single function.');
 		});
 
@@ -690,5 +852,45 @@ describe('Broker', function() {
 
 	}); // End #off/unsubscribe/deregister
 
+
+	describe('other functions', function() {
+		it('getChannels should return a list of channels', function() {
+			let list = broker.getChannels();
+			assert(list, 'List object was empty.');
+		});
+
+		it('getSubscribers with a channel should return that channel', function() {
+
+			let subId = broker.on('bob', function(){});
+			broker.on('smith:james',  function(){});
+
+			let list = broker.getSubscribers('bob');
+
+			assert(list[subId], 'List returned an empty list.');
+			assert.equal(util.inspect(broker.getSubscribers('blah')), '{}', 'Non existant channel did not return empty list.');
+		});
+
+		it('getSubscription should return something', function() {
+
+			let subId = broker.on('bob', function(){});
+
+			let ret = broker.getSubscription(subId);
+
+			assert.equal(ret.subId, subId, 'Wrong subscription was returned.');
+			assert(!broker.getSubscription('bleh'), 'Empty subscription was not returned.');
+		});
+
+		it('clear removes subscriptions and channels', function() {
+
+			let subId = broker.on('bob', function(){});
+			let ret = broker.getSubscription(subId);
+			assert.equal(ret.subId, subId, 'Wrong subscription was returned.');
+
+			broker.clear();
+
+			assert(!broker.getSubscription('bob'), 'Empty subscription was not returned.');
+		});
+
+	});
 
 });
